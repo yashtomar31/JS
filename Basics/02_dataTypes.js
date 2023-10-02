c00fb9122397052a0878d87c92d9d8342334c82e
yashtomar31 <yash16122@iiitd.ac.in>
2023-10-02 07:24:10 +0000

    Data types

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treate all JS code as new version
+
+//alert("hello") // this won't work here coz not binded to html yet, where as works on chrome console coz binded to engine there
+// avoid semi colons
+// code readability should be high
+// mdn documentation not original but better readibility
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+//original documentation - tc39 https://tc39.es/ecma262/
+
+let name = "yash" // string
+let age = 24 // number range <= 2^53 more than that you can use bigint
+let isLoggedin = true // boolean
+ null //- standalone value
+ undefined // - undefined and null are different, undefined is value not assigned, null is representation of empty value
+ Symbol // mainly in react, used in uniqueness
+ 
+ // object - covered in more modules
+
+ console.log(typeof "age");
+ console.log(typeof number);
+ console.log(typeof null); //object as output
+ console.log(typeof undefined); // undefined as output
+
