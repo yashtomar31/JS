12951a2f3ad855f6dd91184858d7471a2609fdc5
yashtomar31 <yash16122@iiitd.ac.in>
2023-10-06 05:17:40 +0000

    Loop starter

---
+++ 05_iterations/01_basics.js
@@ -0,0 +1,8 @@
+
+let array = [10,12,13,14,'yash']
+for (let index = 0; index < array.length; index++) { // basic for loop example
+    const element = array[index];
+    console.log(element)
+}
+
+// breal and continue there in JS
